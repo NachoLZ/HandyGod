a134746f59a1157d22293590d363c4cdee66bc02
Ignacio Lizana <ignacio.lizanaa@sansano.usm.cl>
2020-11-15 22:23:14 -0300

    Working hand alpha

--- Assets/Scripts/HandCursor/CursorManager.cs
+++ Assets/Scripts/HandCursor/CursorManager.cs
@@ -1,32 +1,81 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    [SerializeField] private Texture2D[] cursorTextureArray;
-    [SerializeField] private int frameCount;
-    [SerializeField] private float frameRate;
+public class CursorManager : MonoBehaviour {
+
+    public static CursorManager Instance { get; private set; }
+
+    public event EventHandler<OnCursorChangedEventArgs> OnCursorChanged;
+    public class OnCursorChangedEventArgs : EventArgs {
+        public CursorType cursorType;
+    }
+
+    [SerializeField] private List<CursorAnimation> cursorAnimationList;
+
+    private CursorAnimation cursorAnimation;
+
     private int currentFrame;
     private float frameTimer;
-    
+    private int frameCount;
 
-    private void Start()
-    {
-
-        Cursor.SetCursor(cursorTextureArray[0], new Vector2(140, 140), CursorMode.Auto);    
+    public enum CursorType {
+        Arrow,
+        Grab,
+        Select,
+        Attack,
+        Move
     }
 
-    private void Update()
-    {
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Start() {
+        SetActiveCursorType(CursorType.Arrow);
+    }
+
+    private void Update() {
         frameTimer -= Time.deltaTime;
-        if (frameTimer <= 0f)
-        {
-            frameTimer += frameRate;
+        if (frameTimer <= 0f) {
+            frameTimer += cursorAnimation.frameRate;
             currentFrame = (currentFrame + 1) % frameCount;
-            Cursor.SetCursor(cursorTextureArray[currentFrame], new Vector2(140, 140), CursorMode.Auto);
+            Cursor.SetCursor(cursorAnimation.textureArray[currentFrame], cursorAnimation.offset, CursorMode.Auto);
         }
-        
     }
+
+    public void SetActiveCursorType(CursorType cursorType) {
+        SetActiveCursorAnimation(GetCursorAnimation(cursorType));
+        OnCursorChanged?.Invoke(this, new OnCursorChangedEventArgs { cursorType = cursorType });
+    }
+
+    private CursorAnimation GetCursorAnimation(CursorType cursorType) {
+        foreach (CursorAnimation cursorAnimation in cursorAnimationList) {
+            if (cursorAnimation.cursorType == cursorType) {
+                return cursorAnimation;
+            }
+        }
+        // Couldn't find this CursorType!
+        return null;
+    }
+
+    private void SetActiveCursorAnimation(CursorAnimation cursorAnimation) {
+        this.cursorAnimation = cursorAnimation;
+        currentFrame = 0;
+        frameTimer = 0f;
+        frameCount = cursorAnimation.textureArray.Length;
+    }
+
+
+    [System.Serializable]
+    public class CursorAnimation {
+
+        public CursorType cursorType;
+        public Texture2D[] textureArray;
+        public float frameRate;
+        public Vector2 offset;
+
+    }
+
 }
