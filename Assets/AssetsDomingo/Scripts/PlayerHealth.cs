726c99df689db860ce7f91a2e881ead28825bf49
Dhurmir <domingo.benoit@sansano.usm.cl>
2020-11-24 16:33:21 -0300

    Player Health Status added

---
+++ Assets/AssetsDomingo/Scripts/PlayerHealth.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    [SerializeField]
+    PlayerScript playerScript = default;
+
+    [SerializeField]
+    int numOfHearts;
+
+    [SerializeField]
+    Image[] hearts;
+
+    [SerializeField]
+    Sprite fullHeart;
+
+    [SerializeField]
+    Sprite emptyheart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("PlayerHealthScript Starting now");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerScript.health > numOfHearts)
+        {
+            numOfHearts = playerScript.health;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < playerScript.health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyheart;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+        
+    }
+}
