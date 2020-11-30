db1398bd87fdc29435083fa30631d529878537ac
Lawliet-Kira <diego.reinoso@sansano.usm.cl>
2020-11-30 00:46:56 -0300

    arbol explotando

---
+++ Assets/Apache Files/Scripts/ExplotableScripts/DestructableRigidBody.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableRigidBody : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 forceDirection;
+
+    [SerializeField]
+    float torque;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        float randTorque = Random.Range(-50, 50);
+        float randForceX = Random.Range(forceDirection.x - 50, forceDirection.x + 50);
+        float randForceY = Random.Range(forceDirection.y, forceDirection.y + 50);
+
+        forceDirection.x = randForceX;
+        forceDirection.y = randForceY;
+
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(forceDirection);
+        rb2d.AddTorque(randTorque);
+
+    }
+
+}
---
+++ Assets/Apache Files/Scripts/ExplotableScripts/ExplotableThing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplotableThing : MonoBehaviour
+{
+    [SerializeField]
+    int health = 2;
+
+    [SerializeField]
+    Object destructableRef;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+    }
+
+    private void OnMouseDown() {
+        
+        health--;
+
+        if (health <= 0) {
+            ExplodeThisGameObject();
+        }
+
+    }
+
+    private void ExplodeThisGameObject() {
+
+        GameObject destructable = (GameObject)Instantiate(destructableRef);
+
+        // map the newly loaded destructable object to the x and y position of the previously destroyed barrel
+        destructable.transform.position = transform.position;
+
+        Destroy(gameObject);
+
+    }
+
+}
