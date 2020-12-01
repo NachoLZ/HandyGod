01e9734e0cbe1dd02cd31ed10a9b175a12437121
Ignacio Lizana <ignacio.lizanaa@sansano.usm.cl>
2020-11-30 21:56:53 -0300

    nuevo personaje y fondo

--- Assets/NachoAssets/Scripts/CursorParticle.cs
+++ Assets/NachoAssets/Scripts/CursorParticle.cs
@@ -6,6 +6,8 @@ public class CursorParticle : MonoBehaviour
 
     private Vector3 mousePosition;
     public float moveSpeed = 0.1f;
+    
+    
 
     // Use this for initialization
     void Start()
@@ -16,10 +18,12 @@ public class CursorParticle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-            mousePosition = Input.mousePosition;
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        Vector3 position = transform.position;
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector3.Lerp(transform.position, mousePosition, moveSpeed);
+        position[2] = -1; // the Z value
+        transform.position = position;
 
 
     }
