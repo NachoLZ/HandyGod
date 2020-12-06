c73c82d761035fdfe6d53239a3342cfc6b5ff9e2
Lawliet-Kira <diego.reinoso@sansano.usm.cl>
2020-12-06 19:29:56 -0300

    Cuerda mejorada

---
+++ Assets/Apache Files/Scripts/Rope3/ropeColliders.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class ropeColliders : MonoBehaviour
+{
+    LineRenderer rope;
+    EdgeCollider2D edgeCollider;
+
+    Vector3 points;
+    Vector2[] points2 = new Vector2[35];
+
+    private void Start() {
+        edgeCollider = this.gameObject.AddComponent<EdgeCollider2D>();
+        rope = this.gameObject.GetComponent<LineRenderer>();
+
+        getNewPositions();
+
+        edgeCollider.points = points2;
+    }
+
+    private void Update() {
+        getNewPositions();
+
+        edgeCollider.points = points2;
+    }
+
+    void getNewPositions() {
+        for (int i = 0; i < rope.positionCount; i++) {
+            points = rope.GetPosition(i);
+            points2[i] = new Vector2(points.x, points.y);
+        }
+    }
+}
